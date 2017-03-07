a314892b0703a2894909d7c2ad507b5e18397cd1
赖巍 <skyoflw@gmail.com>
2017-03-07 09:51:24 +0800

    friend for model.

---
+++ __init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+from .friend import Friend
+
+__ALL__ = [
+    'Friend'
+]
---
+++ _core.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+class _BaseModel:
+    def __init__(self, data: dict):
+        if not isinstance(data, dict):
+            raise TypeError
+        self._data = data
+
+    def _get(self, key): return self._data[key]
+
